548ad9e97422a95a780ba388ab3b3785feb42d1a
Shimesaba <Shimesaba@shimesaba-8.local>
2017-04-05 00:33:59 +0900

    update: ex4.py but it's not fixed

--- ex4.py
+++ ex4.py
@@ -4,11 +4,20 @@
 # after [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
 
 #arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-arr = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+#arr = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+arr = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]		# 3x4 のループ
 arr2 = []
+
 for i in range(len(arr)):
     arr2.append([])
+print(arr2)
+for i in range(len(arr)):
+    #arr2.append([])
     for j in range(len(arr[i])):
+        print(len(arr[i]))
         arr2[i].append(arr[j][i])
+        print("i : %s" %(i))
+        print("j : %s" %(j))
+        print(arr2)
 print(arr)
 print(arr2)
